9d5799faa71a742146114b7ca33802a7e7f5200d
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-25 14:58:51 +0530

    Add files via upload

---
+++ Sum of 5.py
@@ -0,0 +1,7 @@
+#Take five numbers and display their sum
+a = int(input("Enter a Number a"))
+b = int(input("Enter a Number b"))
+c = int(input("Enter a Number c"))
+d = int(input("Enter a Number d"))
+e = int(input("Enter a Number e"))
+print(a+b+c+d+e)
