67e8e14f9d91d15a55b4aaabc31408f17ccd4931
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-25 16:21:32 +0530

    Add files via upload

---
+++ Arithmatic op. for 3 digits.py
@@ -0,0 +1,12 @@
+#Perform all arithmetic operations on three numbers
+a = int(input("Enter a number a"))
+b = int(input("Enter a number b"))
+c = int(input("Enter a number c"))
+result1=a+b+c
+print("sum", result1)
+result2=a-b-c
+print("difference", result2)
+result3=a*b*c
+print("multiply", result3)
+result4=a/b/c
+print("divide", result4)
