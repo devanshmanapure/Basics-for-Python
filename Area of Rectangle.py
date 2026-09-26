a4f71c831161545692da579693b254aecd546ee6
Devansh Manapure <devansh.manapure@gmail.com>
2026-09-26 11:35:12 +0530

    Add files via upload

---
+++ Area of Rectangle.py
@@ -0,0 +1,5 @@
+#Calculate area of a rectangle
+l = int(input("Enter the length of the reactangle"))
+b = int(input("Enter the breath of the reactangle"))
+result = l*b
+print("area of rectangle is",result)
---
+++ Percentage.py
@@ -0,0 +1,8 @@
+#Calculate percentage in examination
+a = int(input("Enter marks of Maths Subject"))
+b = int(input("Enter marks of English Subject"))
+c = int(input("Enter marks of Science Subject"))
+d = int(input("Enter marks of Sanskrit Subject"))
+e = int(input("Enter marks of History Subject"))
+result = 100*(a+b+c+d+e)/500
+print("Persentage is", result)
